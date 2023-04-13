0e10bfa236037adfa4b250523a909b289a5db05d
Reddy <850066873@genpact.com>
2023-04-13 12:55:23 +0530

    pranav subcription

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Plans from './Components/Plans/Plans';
+function App() {
+  return (
+    <div className="App">
+        <Plans/>
+    </div>
+  );
+}
+
+export default App;
